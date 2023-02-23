4c16011c63bf40b357b41daebd3d2b682894929a
Robert Carter Mills <robcmills@gmail.com>
2023-02-22 23:46:17 -0800

    Add css language server

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -107,6 +107,13 @@ lspconfig.tsserver.setup {
   -- },
 }
 
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.cssls.setup {
+  capabilities = capabilities,
+}
 
 -- diagnostics
 local signs = {
